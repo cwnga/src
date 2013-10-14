47cc0f335618adc99647ddf29565b5b7e687602f
ng_chi_wa@hotmail.com <ng_chi_wa@hotmail.com>
2013-10-14 22:59:30 +0800

    add 05

---
+++ frontend/test/05_getting_data/mojits/Twitter/controller.server.js
@@ -0,0 +1,71 @@
+/*jslint anon:true, sloppy:true, nomen:true*/
+YUI.add('Twitter', function(Y, NAME) {
+
+/**
+ * The Twitter module.
+ *
+ * @module Twitter
+ */
+
+    /**
+     * Constructor for the Controller class.
+     *
+     * @class Controller
+     * @constructor
+     */
+    Y.namespace('mojito.controllers')[NAME] = {
+
+        /**
+         * Method corresponding to the 'index' action.
+         *
+         * @param ac {Object} The ActionContext that provides access
+         *        to the Mojito API.
+         */
+        index: function(ac) {
+            /// ac.models.get('TwitterModelFoo').getData(function(err, data) {
+            ///     if (err) {
+            ///         ac.error(err);
+            ///         return;
+            ///     }
+            ///     ac.assets.addCss('./index.css');
+            ///     ac.done({
+            ///         status: 'Mojito is working.',
+            ///         data: data
+            ///     });
+            /// });
+
+       var q="@yuilibrary", oauth_keys, count=10;
+
+            // Get Twitter API keys from your developer account (https://dev.twitter.com/apps) and
+            // use the `oauth_keys` to hold your consumer key/secret and access token/secret.
+            // If you leave `oauth_keys` undefined, your app will just use mocked data.
+            /*
+             * oauth_keys = {
+             *    "consumer_key": "xxxx",
+             *    "consumer_secret": "xxxx",
+             *    "access_token_key": "xxxx",
+             *    "access_token_secret": "xxxx"
+             * }
+            */
+
+            // Get OAuth keys from definition.json to get real data.
+            // If `oauth_keys==null`, use mock data from model.
+            ac.models.get('TwitterSearchModel').getData(count, q, oauth_keys, function (err, data) {
+              if (err) {
+                ac.error(err);
+                return;
+              }
+              // add mojit specific css
+              ac.assets.addCss('./index.css');
+              ac.done({
+                title: "YUI Twitter Mentions",
+                results: data.statuses
+              });
+            });
+
+
+        }
+
+    };
+
+}, '0.0.1', {requires: ['mojito', 'mojito-assets-addon', 'mojito-models-addon']});
